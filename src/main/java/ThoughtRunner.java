52b30154b7a9febc31fc22cdd5c181e8f2838062
Patrick Whelan <patrickawhelan@gmail.com>
2020-03-30 11:31:06 -0400

    final code from 3/30/2020

--- src/main/java/ThoughtRunner.java
+++ src/main/java/ThoughtRunner.java
@@ -7,6 +7,30 @@ import org.hibernate.service.ServiceRegistry;
 public class ThoughtRunner {
 
   public static void main(String[] args) {
+    Configuration configuration = new Configuration();
+    configuration.configure("hibernate.cfg.xml");
+    configuration.addAnnotatedClass(Thought.class);
 
+    // Create the Hibernate service Registry
+    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+        applySettings(configuration.
+            getProperties()).
+        build();
+// instantiate the session factory
+    SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    Session session = sessionFactory.getCurrentSession();
+    session.beginTransaction();
+    Thought thought = new Thought();
+    thought.setDescription("Hibernate is hard to set up, but easy to work with");
+    thought.setRating(10);
+    Thought thoughtTheSecond = new Thought();
+    thoughtTheSecond.setDescription("Second thought is better than the first");
+    thoughtTheSecond.setRating(1000);
+    session.save(thought);
+    session.save(thoughtTheSecond);
+    session.getTransaction().commit();
+    System.out.println(thought.getId());
+    System.out.println(thoughtTheSecond.getId());
+    session.close();
   }
 }
